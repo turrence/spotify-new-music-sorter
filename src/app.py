cd1d2d359ab53c072d3d62dfd1f7d1deb51a4dda
Terence Tong <tjtong2006@gmail.com>
2020-04-01 02:07:00 -0700

    rm extra spacing

--- src/app.py
+++ src/app.py
@@ -6,9 +6,7 @@ from datetime import datetime as dt
 from datetime import timezone as tz
 
 TARGET_PLAYLIST = playlist.get_target_playlist(dt.now(tz=tz.utc))
-
-PLAYLIST_LAST_UPDATED = playlist.get_newest_date_in_playlist(TARGET_PLAYLIST) # in utc
-
+PLAYLIST_LAST_UPDATED = playlist.get_newest_date_in_playlist(TARGET_PLAYLIST)
 SONGS_TO_BE_ADDED = saved_songs.get_unadded_songs(PLAYLIST_LAST_UPDATED)
 
 if len(SONGS_TO_BE_ADDED) < 1:
