1507a52fd92fec07b6d51d491387b84c73e54b3a
jaasonw <jayywong92@gmail.com>
2020-04-01 16:58:48 -0700

    flask frontend pt 1

--- src/auth_token.py
+++ src/auth_token.py
@@ -1,15 +1,29 @@
-import os
-
 import spotipy
-import spotipy.util as util
+from spotipy.oauth2 import SpotifyOAuth
+from flask import Flask, redirect, request
 
-username = "terence"
-scope = "user-library-read playlist-read-private playlist-modify-private"
+from playlist import get_newest_date_in_playlist
 
-token = util.prompt_for_user_token(username, scope,
-                           client_id=os.environ['SPOTIPY_CLIENT_ID'],
-                           client_secret=os.environ['SPOTIPY_CLIENT_SECRET'],
-                           redirect_uri=os.environ['SPOTIPY_REDIRECT_URI'],
-                           cache_path="cache/.cache-"+username)
+app = Flask(__name__)
+clients = []
 
-sp = spotipy.Spotify(auth=token)
+
+@app.route('/')
+def main():
+    scope = "user-library-read playlist-read-private playlist-modify-private"
+    oauth = SpotifyOAuth(scope=scope, username="temp")
+    # ask the user for authorization here
+    if (len(request.args) == 0):
+        return redirect(oauth.get_authorize_url())
+    # we got the code here, use it to create a client
+    else:
+        print(request.args["code"])
+        token = oauth.get_access_token(request.args["code"], as_dict=False)
+        clients.append(spotipy.Spotify(auth=token))
+        print(get_newest_date_in_playlist(
+            "1VjQ8zUVT6nCx6HKkctISE", clients[0]))
+        return "Successfully authenticated, you may close this now"
+
+
+if __name__ == '__main__':
+    app.run()
