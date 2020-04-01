c06e7c295ba96fec67f8bd90a49590356afe6dd2
Terence Tong <tjtong2006@gmail.com>
2020-04-01 02:03:46 -0700

    handles season transition

--- src/playlist.py
+++ src/playlist.py
@@ -1,4 +1,5 @@
 from datetime import datetime as dt
+from datetime import timezone as tz
 from auth_token import sp, username
 
 import constant
@@ -35,10 +36,10 @@ def get_target_playlist(date) -> str:
         else:
             offset += chunk
 
-    if all_playlists[target_playlist_name] is None:
+    if target_playlist_name not in all_playlists:
         resp = sp.user_playlist_create(sp.me()['id'], target_playlist_name, public=False, 
-            description='AUTOMATED PLAYLIST - MODIFYING THIS PLAYLIST MAY RESULT IN UNEXPECTED BEHAVIORS \
-                (you can change the description tho)')
+            description='AUTOMATED PLAYLIST - MODIFYING THIS PLAYLIST MAY RESULT IN UNEXPECTED BEHAVIORS' +
+            '(you can change the description tho)')
         return resp['id']
     else:
         return all_playlists[target_playlist_name]
@@ -47,8 +48,19 @@ def get_target_playlist(date) -> str:
 def get_newest_date_in_playlist(pl_id):
     # assume chronologically added ordered playlist
     songs = sp.playlist_tracks(pl_id, fields="items, total")
-    if songs['total'] == 0: # what if its an empty playlist
-        return None  
-    # in utc
-    return dt.strptime(songs['items'][songs['total'] - 1]['added_at'], "%Y-%m-%dT%H:%M:%SZ") 
+    if songs['total'] == 0:
+        return start_season_time(dt.now(tz=tz.utc))
+    return dt.strptime(songs['items'][songs['total'] - 1]['added_at'], "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=tz.utc) 
 
+# given a datetime, return a dt of the start of the season
+# for e.g. if its winter 2020, return DEC 1, 2019 00:00 UTC
+# for e.g. if its spring 2020, return MAR 1, 2020 00:00 UTC
+def start_season_time(now) -> dt:
+    if now.month in [12, 1, 2]:
+        return dt(now.year if now.month == 12 else now.year - 1, 12, 1, tzinfo=tz.utc)
+    elif now.month in range(3, 6):
+        return dt(now.year, 3, 1, tzinfo=tz.utc)
+    elif now.month in range(6, 9):
+        return dt(now.year, 6, 1, tzinfo=tz.utc)
+    else:
+        return dt(now.year, 9, 1, tzinfo=tz.utc)
