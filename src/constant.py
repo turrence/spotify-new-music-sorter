5f3a5bfd2c9183a6aa896932bb67252bb60ffce9
jaasonw <jayywong92@gmail.com>
2020-04-01 21:35:13 -0700

    cache path fix

--- src/constant.py
+++ src/constant.py
@@ -1,4 +1,7 @@
+import os
+
 SPRING = "spring"
 SUMMER = "summer"
 WINTER = "winter"
 FALL = "fall"
+CACHE_PATH = os.path.dirname(os.path.realpath(__file__)) + "/../cache"
--- src/web_auth.py
+++ src/web_auth.py
@@ -1,3 +1,4 @@
+import constant
 import spotipy
 import os
 from spotipy.oauth2 import SpotifyOAuth
@@ -11,10 +12,10 @@ auth_server.debug = False
 def main():
     scope = "user-library-read playlist-read-private playlist-modify-private"
     oauth = SpotifyOAuth(
-        scope=scope, 
-        username="temp",
+        scope = scope, 
+        username = "temp",
         # TODO: THIS SHOULD NOT BE OS.GETCWD
-        cache_path=os.getcwd() + "/cache/.cache-temp"
+        cache_path = constant.CACHE_PATH + "/.cache-temp"
     )
     # ask the user for authorization here
     if ("code" not in request.args):
@@ -24,7 +25,7 @@ def main():
         print(request.args["code"])
         token = oauth.get_access_token(request.args["code"], as_dict=False)
         ClientManager.clients.append(spotipy.Spotify(auth=token))
-        os.rename(os.getcwd() + "/cache/.cache-temp", os.getcwd() +
-                  "/cache/.cache-" + ClientManager.clients[0].me()['id'])
+        os.rename(constant.CACHE_PATH + "/.cache-temp", constant.CACHE_PATH +
+                  "/.cache-" + ClientManager.clients[0].me()['id'])
         print(ClientManager.clients[0].me()['id'])
         return "Successfully authenticated, you may close this now"
