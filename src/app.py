d69a4d04ed19858b54a92c0d456d7cd824522361
jaasonw <jayywong92@gmail.com>
2020-04-04 14:57:45 -0700

    move the update playlists function to playlist.py

--- src/app.py
+++ src/app.py
@@ -19,6 +19,8 @@ class App(object):
             os.mkdir(constant.CACHE_PATH)
         self.update_clients()
     
+    # Refreshes the access tokens and updates the playlists for all clients in 
+    # the cache
     def update_clients(self):
         for filename in os.listdir(constant.CACHE_PATH):
             id = filename[len(".cache-"):]
@@ -32,17 +34,7 @@ class App(object):
                 redirect_uri = config.redirect_uri
             )
             token = oauth.get_cached_token()['access_token']
-            self.update_playlist(spotipy.Spotify(auth = token))
-
-    def update_playlist(self, sp):
-        target_playlist = playlist.get_target_playlist(dt.now(tz=tz.utc), sp)
-        # in utc
-        last_updated = playlist.get_newest_date_in_playlist(target_playlist, sp)
-        songs_to_be_added = saved_songs.get_unadded_songs(last_updated, sp)
-        if len(songs_to_be_added) < 1:
-            print("no songs to be added for", sp.me()['id'])
-        else:
-            sp.user_playlist_add_tracks(sp.me()['id'], target_playlist, songs_to_be_added)
+            playlist.update_playlist(spotipy.Spotify(auth=token))
 
     def run_periodically(self):
         # update every 10 minutes
