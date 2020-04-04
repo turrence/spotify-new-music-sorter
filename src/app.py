149032820b0649fe178d245af530360fc9b2d517
jaasonw <jayywong92@gmail.com>
2020-04-03 22:53:22 -0700

    oops there was more merge conflict

--- src/app.py
+++ src/app.py
@@ -34,16 +34,6 @@ class App(object):
             token = oauth.get_cached_token()['access_token']
             self.update_playlist(spotipy.Spotify(auth = token))
 
-<<<<<<< HEAD
-    def update_playlists(self):
-        for c in ClientManager.clients:
-            target_playlist = playlist.get_target_playlist(dt.now(tz=tz.utc), c)
-            # in utc
-            last_updated = playlist.get_newest_date_in_playlist(target_playlist, c)
-            songs_to_be_added = saved_songs.get_unadded_songs(last_updated, c)
-            if len(songs_to_be_added) >= 1:
-                c.user_playlist_add_tracks(c.me()['id'], target_playlist, songs_to_be_added)
-=======
     def update_playlist(self, sp):
         target_playlist = playlist.get_target_playlist(dt.now(tz=tz.utc), sp)
         # in utc
@@ -53,23 +43,10 @@ class App(object):
             print("no songs to be added for", sp.me()['id'])
         else:
             sp.user_playlist_add_tracks(sp.me()['id'], target_playlist, songs_to_be_added)
->>>>>>> daae0f884059e1340da922013f53d840143c8a27
 
     def run_periodically(self):
         # update every 10 minutes
         threading.Timer(constant.UPDATE_FREQUENCY, self.run_periodically).start()
-<<<<<<< HEAD
-        try:
-            self.clients.refresh_clients()
-            self.update_playlists()
-        except Exception as e:
-            with open(constant.SRC_PATH + '/../error.log', 'a') as f:
-                f.write(str(e))
-                f.write(traceback.format_exc())
-
-app = App()
-app.run_periodically()
-=======
         print("Updating playlists....")
         self.update_clients()
 
@@ -79,4 +56,3 @@ if __name__ == "__main__":
     # run the server in a background thread
     server_thread = threading.Thread(target = auth_server.run, kwargs=dict(port=config.port))
     server_thread.run() 
->>>>>>> daae0f884059e1340da922013f53d840143c8a27
