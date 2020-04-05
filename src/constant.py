0036973f420e4d50d3bd67823b8dc47c0fbe4c30
jaasonw <jayywong92@gmail.com>
2020-04-04 18:37:01 -0700

    update permissions

--- src/constant.py
+++ src/constant.py
@@ -5,5 +5,5 @@ SUMMER = "summer"
 WINTER = "winter"
 FALL = "fall"
 CACHE_PATH = os.path.dirname(os.path.realpath(__file__)) + "/../cache"
-SCOPE = "user-library-read playlist-read-private playlist-modify-private"
+SCOPE = "user-library-read playlist-read-private playlist-modify-private playlist-modify-public"
 UPDATE_FREQUENCY = 10 # in seconds
