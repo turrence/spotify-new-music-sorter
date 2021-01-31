f8d09c89add5ee0476a6f3d516cbf973363688ad
jaasonw <jayywong92@gmail.com>
2021-01-31 15:45:02 -0800

    wait, wrong path

--- src/constant.py
+++ src/constant.py
@@ -9,5 +9,5 @@ SRC_PATH = os.path.dirname(os.path.realpath(__file__))
 CACHE_PATH = SRC_PATH + "/../cache"
 SCOPE = "user-library-read playlist-read-private playlist-modify-private playlist-modify-public"
 UPDATE_FREQUENCY = 600 # in seconds
-DATABASE_NAME = SRC_PATH + "/database.db"
+DATABASE_NAME = SRC_PATH + "/../database.db"
 ERROR_THRESHOLD = 30
